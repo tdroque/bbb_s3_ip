5f18f4374247b9fa4db12b9def7acb8fb552fb92
tdroque <tdroque@gmail.com>
2020-05-06 20:42:26 -0400

    Computer Python File

---
+++ ip_s3_down.py
@@ -0,0 +1,11 @@
+# Download file from S3
+import boto3
+
+ACCESS_KEY = '' # aws account access key
+SECRET_ACCESS_KEY = '' # aws account secret access key
+BUCKET_NAME = '' # aws bucket name
+DOWNLOAD_FILE = '' # s3 file name to download
+
+s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_ACCESS_KEY)
+s3.download_file(BUCKET_NAME, DOWNLOAD_FILE, 'ip.txt') # s3 bucket name, file to download, saved file name
+
